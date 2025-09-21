130974aa8004ab5d83dff1a95f7561c4095c0bec
Iaroslav Iatsenko <yaryat@ua.fm>
2025-09-21 17:22:41 +0300

    first commit

---
+++ tests/conftest.py
@@ -0,0 +1,19 @@
+# conftest module for pytest
+import os
+import sys
+import pytest
+
+sys.path.append(os.getcwd())
+
+@pytest.fixture()
+def spark():
+    try:
+        from databricks.connect import DatabricksSession
+        spark = DatabricksSession.builder.getOrCreate()
+    except ImportError:
+        try:
+            from pyspark.sql import SparkSession
+            spark = SparkSession.builder.getOrCreate()
+        except:
+            raise ImportError("Neither Databricks Session or Spark Session are available") 
+    return spark
